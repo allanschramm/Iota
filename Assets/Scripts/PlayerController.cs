ab3bc799671489037f224522432b0d6817a07ca2
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-05-07 23:06:47 -0300

    New player variables

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float maxSpeed;
+    public float jumpForce;
+
+    private bool grounded;
+    private bool jumping;
+
+    private Rigidbody2D rb2d;
+    private Animator animator;
+    private SpriteRenderer sprite;
+
     // Start is called before the first frame update
     void Start()
     {

28f06ea231ecb69e33a17f84a5eb014193b0904c
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-05-09 18:39:51 -0300

    Attack controller

---
+++ Assets/Scripts/AttackController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D> ().velocity
+            = new Vector2 (transform.localRotation.eulerAngles.z == 0 ? 20 : -20, 0);
+        Destroy (gameObject, .5f);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other) {
+        if (!other.isTrigger) {
+            Destroy (gameObject);
+        }       
+    }
+}

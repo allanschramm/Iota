4dc28f7d29b9a5fe43108560678a2d9c1f38a3cd
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-05-11 19:46:52 -0300

    Save new HUD

---
+++ Assets/Scripts/Player/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthBar : MonoBehaviour
+{
+    public Sprite[] bar;
+    public Image healthBarUI;
+
+    private PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find ("Alien").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBarUI.sprite = bar[player.health];
+    }
+}

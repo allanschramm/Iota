8149df567ffe021ee3a25baca09246516a1bd882
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-05-11 10:41:19 -0300

    Changing jump hotkey

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -20,7 +20,7 @@ public class PlayerMovement : MonoBehaviour {
 
 		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-		if (Input.GetKeyDown(KeyCode.Z))
+		if (Input.GetButton("Jump"))
 		{
 			jump = true;
 		}

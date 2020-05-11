85001065a54b1870f9b87eb05132fd0b609819c3
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-05-11 10:40:55 -0300

    Grabbing weapons

---
+++ Assets/AxeGrab.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AxeGrab : MonoBehaviour
+{
+    public int GunNumber;
+    public int WeaponNumber;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag ("Player")) {
+            // other.GetComponent<PlayerMovement> ().EnableGun (GunNumber);
+            // other.GetComponent<PlayerMovement> ().EnableWeapon (WeaponNumber);
+            Destroy (gameObject);
+        }
+    }
+}
---
+++ Assets/SickleGrab.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SickleGrab : MonoBehaviour
+{
+    public int GunNumber;
+    public int WeaponNumber;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag ("Player")) {
+            // other.GetComponent<PlayerMovement> ().EnableGun (GunNumber);
+            // other.GetComponent<PlayerMovement> ().EnableGun (WeaponNumber);
+            Destroy (gameObject);
+        }
+    }
+}

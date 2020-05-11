da7767f05a4a4874d35ca5cfcb4dede775eafaac
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-05-11 18:02:37 -0300

    Enemy killing player

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -18,6 +18,24 @@ public class EnemyController : MonoBehaviour
     private bool attacking = false;
     private GameObject player;
 
+ public float life = 10;
+	private bool isPlat;
+	private bool isObstacle;
+	private Transform fallCheck;
+	private Transform wallCheck;
+	public LayerMask turnLayerMask;
+	private Rigidbody2D rb;
+
+	// private bool facingRight = true;
+	public bool isInvincible = false;
+	private bool isHitted = false;
+
+    void Awake () {
+		fallCheck = transform.Find("FallCheck");
+		wallCheck = transform.Find("WallCheck");
+		rb = GetComponent<Rigidbody2D>();
+	}
+
     // Start is called before the first frame update
     void Start()
     {
@@ -47,6 +65,13 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    void FixedUpdate() {
+        if (life <= 0) {
+			transform.GetComponent<Animator>().SetBool("IsDead", true);
+			StartCoroutine(DestroyEnemy());
+		}
+    }
+
     void Move(){
         Vector2 newPosition = transform.position;
         newPosition.x += direction * speed * Time.deltaTime;
@@ -70,6 +95,27 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    public void ApplyDamage(float damage) {
+		if (!isInvincible) 
+		{
+			float direction = damage / Mathf.Abs(damage);
+			damage = Mathf.Abs(damage);
+			transform.GetComponent<Animator>().SetBool("Hit", true);
+			life -= damage;
+			rb.velocity = Vector2.zero;
+			rb.AddForce(new Vector2(direction * 500f, 100f));
+			StartCoroutine(HitTime());
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Player" && life > 0)
+		{
+			collision.gameObject.GetComponent<CharacterController2D>().ApplyDamage(2f, transform.position);
+		}
+	}
+
     void Flip(){
         Vector3 escala = transform.localScale;
         escala.x *= -1;
@@ -79,4 +125,25 @@ public class EnemyController : MonoBehaviour
     public void AttackPlayer(){
         attacking = true;
     }
+
+    	IEnumerator HitTime()
+	{
+		isHitted = true;
+		isInvincible = true;
+		yield return new WaitForSeconds(0.1f);
+		isHitted = false;
+		isInvincible = false;
+	}
+
+    IEnumerator DestroyEnemy()
+	{
+		CapsuleCollider2D capsule = GetComponent<CapsuleCollider2D>();
+		capsule.size = new Vector2(1f, 0.25f);
+		capsule.offset = new Vector2(0f, -0.8f);
+		capsule.direction = CapsuleDirection2D.Horizontal;
+		yield return new WaitForSeconds(0.25f);
+		rb.velocity = new Vector2(0, rb.velocity.y);
+		yield return new WaitForSeconds(3f);
+		Destroy(gameObject);
+	}
 }

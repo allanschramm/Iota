c44dc64019c481dcd77ddf4ddbb108fd0c3d99b6
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-05-20 18:45:19 -0300

    Fixing player death

--- Assets/Scripts/Player/CharacterController2D.cs
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -399,7 +399,7 @@ public class CharacterController2D : MonoBehaviour
 		animator.SetBool("IsDead", true);
 		canMove = false;
 		invincible = true;
-		GetComponent<Attack>().enabled = false;
+		canAttack = false;
 		yield return new WaitForSeconds(0.4f);
 		m_Rigidbody2D.velocity = new Vector2(0, m_Rigidbody2D.velocity.y);
 		yield return new WaitForSeconds(1.1f);

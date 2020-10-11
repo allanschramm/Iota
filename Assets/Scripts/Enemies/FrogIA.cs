fb9419fcfad6fa99f8c76ddaa6307e835d9503e2
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-10-11 19:57:20 -0300

    Update FrogIA.cs

--- Assets/Scripts/Enemies/FrogIA.cs
+++ Assets/Scripts/Enemies/FrogIA.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class FrogIA : MonoBehaviour
 {
+    private GameController _GameController;
+    private Rigidbody2D frogRb;
+    private Animator frogAnim;
+
+    public float speed;
+    public float timeToWalk;
+
+
     // Start is called before the first frame update
     void Start()
     {
         
+        _GameController = FindObjectOfType<GameController>();
+
     }
 
     // Update is called once per frame

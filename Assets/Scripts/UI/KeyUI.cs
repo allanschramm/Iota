63de2546124ce8b3aff283fe4da9637661143831
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-10-11 03:01:33 -0300

    Key collect script

--- Assets/Scripts/UI/KeyUI.cs
+++ Assets/Scripts/UI/KeyUI.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyUI : MonoBehaviour
 {
+    public Sprite[] bar;
+    public Image Key;
+
+    private CharacterController2D player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find ("Player").GetComponent<CharacterController2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Key.sprite = bar[player.keys];
     }
 }

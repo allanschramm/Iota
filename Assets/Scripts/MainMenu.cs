ebc9fef7d618f2fcb86350f31c1e0f30f2627316
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-10-11 00:38:35 -0300

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
+    public void QuitGame(){
+        Debug.Log("Quitting");
+        Application.Quit();
     }
 }

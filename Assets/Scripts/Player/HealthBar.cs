7374a3a2880ad0eae840916f0c7d2e98013f4db4
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-10-11 02:21:35 -0300

    New life count UI script

--- Assets/Scripts/Player/HealthBar.cs
+++ Assets/Scripts/Player/HealthBar.cs
@@ -7,11 +7,11 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Sprite[] bar;
-    public Image healthBarUI;
+    public Image healthBarUI1;
+    public Image healthBarUI2;
+    public Image healthBarUI3;
 
     private CharacterController2D player;
-    private CharacterController2D playerNew;
-
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +22,22 @@ public class HealthBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        healthBarUI.sprite = bar[player.life];
+        if(player.life >= 0 && player.life <= 4){
+            healthBarUI1.sprite = bar[player.life];
+        }
+
+        if(player.life >= 5 && player.life <= 8){
+            healthBarUI2.sprite = bar[player.life-4];
+        }
+        else if(player.life <= 4){
+            healthBarUI2.sprite = bar[0];
+        }
+
+        if(player.life >= 9 && player.life <= 12){
+            healthBarUI3.sprite = bar[player.life-8];
+        }
+        else if(player.life <= 8){
+            healthBarUI3.sprite = bar[0];
+        }
     }
 }

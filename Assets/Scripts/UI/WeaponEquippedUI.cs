51d1d362038d6e2eaf2e425d4412be32f6f6c7ff
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-10-11 14:16:55 -0300

    Update WeaponEquippedUI.cs

--- Assets/Scripts/UI/WeaponEquippedUI.cs
+++ Assets/Scripts/UI/WeaponEquippedUI.cs
@@ -6,19 +6,21 @@ using UnityEngine.UI;
 public class WeaponEquippedUI : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Weapon weapon;
+    public Weapon weaponEquipped;
     private SpriteRenderer sprite;
+
+    public Sprite bar;
 
     // Start is called before the first frame update
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
-        sprite.sprite = weapon.image;
+        sprite.sprite = weaponEquipped.image;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bar = sprite.sprite;
     }
 }

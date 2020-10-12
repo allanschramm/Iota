6b9b901d43e638dbe70ffe7c99bb6a8ec7157bfb
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-10-11 22:11:08 -0300

    New frog IA

--- Assets/Scripts/Enemies/FrogIA.cs
+++ Assets/Scripts/Enemies/FrogIA.cs
@@ -11,18 +11,82 @@ public class FrogIA : MonoBehaviour
     public float speed;
     public float timeToWalk;
 
+    public GameObject HitBox;
+    public bool isLookingLeft;
+
+    private int h;
+
 
     // Start is called before the first frame update
     void Start()
     {
         
-        _GameController = FindObjectOfType<GameController>();
+        _GameController = FindObjectOfType(typeof(GameController)) as GameController;
+
+        frogRb = GetComponent<Rigidbody2D>();
+        frogAnim = GetComponent<Animator>();
+
+        StartCoroutine("frogWalk");
 
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if(h > 0 && isLookingLeft == true){
+            Flip();
+        }
+        else if (h < 0 && isLookingLeft == false){
+            Flip();
+        }
         
+        frogRb.velocity = new Vector2(h * speed, frogRb.velocity.y);
+
+        if(h != 0){
+            frogAnim.SetBool("IsWalk", true);
+        }
+        else{
+            frogAnim.SetBool("IsWalk", false);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        if(col.gameObject.tag == "HitBox"){
+            h = 0;
+            StopCoroutine("frogWalk");
+            Destroy(HitBox);
+            // _GameController.PlaySFX(_GameController.sfxEnemyDead, 0.2f);
+            frogAnim.SetTrigger("IsDead");
+        }
+    }
+
+    IEnumerator frogWalk(){
+        int rand = Random.Range(0, 100);
+
+        if(rand < 33){
+            h = -1;
+        }
+        else if(rand < 66){
+            h = 0;
+        }
+        else{
+            h = 1;
+        }
+
+        yield return new WaitForSeconds(timeToWalk);
+        StartCoroutine("frogWalk");
+    }
+
+    void OnDead(){
+        Destroy(this.gameObject);
+    }
+
+    void Flip(){
+        isLookingLeft = !isLookingLeft;
+        float x = transform.localScale.x * -1;
+        transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+
     }
 }

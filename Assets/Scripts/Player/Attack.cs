4845fd622a0be8e60995c4fb954b45fc1dbeb0b7
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-10-12 16:35:57 -0300

    Update Attack.cs

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -5,20 +5,7 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
 	public float damage;
-	// public GameObject throwableObject;
-	// public Transform attackCheck;
-	// private Rigidbody2D m_Rigidbody2D;
 	public Animator animator;
-
-	// public bool canAttack = true;
-	// public bool isTimeToCheck = false;
-
-	// public GameObject cam;
-
-	// private void Awake()
-	// {
-	// 	m_Rigidbody2D = GetComponent<Rigidbody2D>();
-	// }
 
 	// Start is called before the first frame update
 	void Start()
@@ -35,7 +22,7 @@ public class Attack : MonoBehaviour
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		EnemyController enemy = other.GetComponent<EnemyController> ();
+		FrogIA enemy = other.GetComponent<FrogIA> ();
 		DestructibleObject destrObject = other.GetComponent<DestructibleObject> ();
 		
 		if(enemy != null){
@@ -46,55 +33,4 @@ public class Attack : MonoBehaviour
 			destrObject.ApplyDamage(damage);
 		}
 	}
-
-
-    // // Update is called once per frame
-    // void Update()
-    // {
-	// 	if (Input.GetKeyDown(KeyCode.S) && canAttack)
-	// 	{
-	// 		canAttack = false;
-	// 		animator.SetBool("IsAttacking", true);
-	// 		StartCoroutine(AttackCooldown());
-	// 	}
-
-	// 	if (Input.GetKeyDown(KeyCode.D) && canAttack)
-	// 	{
-	// 		canAttack = false;
-	// 		animator.SetBool("IsAttacking", true);
-	// 		StartCoroutine(AttackCooldown());
-	// 	}
-
-	// 	if (Input.GetKeyDown(KeyCode.A))
-	// 	{
-	// 		GameObject throwableWeapon = Instantiate(throwableObject, transform.position + new Vector3(transform.localScale.x * 0.5f,-0.2f), Quaternion.identity) as GameObject; 
-	// 		Vector2 direction = new Vector2(transform.localScale.x, 0);
-	// 		throwableWeapon.GetComponent<ThrowableWeapon>().direction = direction; 
-	// 		throwableWeapon.name = "ThrowableWeapon";
-	// 	}
-	// }
-
-	// IEnumerator AttackCooldown()
-	// {
-	// 	yield return new WaitForSeconds(0.25f);
-	// 	canAttack = true;
-	// }
-
-	// public void DoDashDamage()
-	// {
-	// 	dmgValue = Mathf.Abs(dmgValue);
-	// 	Collider2D[] collidersEnemies = Physics2D.OverlapCircleAll(attackCheck.position, 0.9f);
-	// 	for (int i = 0; i < collidersEnemies.Length; i++)
-	// 	{
-	// 		if (collidersEnemies[i].gameObject.tag == "Enemy")
-	// 		{
-	// 			if (collidersEnemies[i].transform.position.x - transform.position.x < 0)
-	// 			{
-	// 				dmgValue = -dmgValue;
-	// 			}
-	// 			collidersEnemies[i].gameObject.SendMessage("ApplyDamage", dmgValue);
-	// 			cam.GetComponent<CameraFollow>().ShakeCamera();
-	// 		}
-	// 	}
-	// }
 }

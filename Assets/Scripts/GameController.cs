d7303c07fd34ce8244230b773373b6dc663c1381
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-10-11 22:11:16 -0300

    New game controller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Audio")]
+    public AudioSource sfxSource;
+    public AudioSource musicSource;
+
+    public AudioClip sfxJump;
+    public AudioClip sfxAttack;
+    public AudioClip sfxWeapons;
+    public AudioClip[] sfxStep;
+    public AudioClip sfxEnemyDead;
+
     // Start is called before the first frame update
     void Start()
     {

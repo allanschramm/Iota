59efaac6abd027a3558531f947ae545771480c33
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-10-24 20:36:37 -0300

    Update FrogIA.cs

--- Assets/Scripts/Enemies/FrogIA.cs
+++ Assets/Scripts/Enemies/FrogIA.cs
@@ -22,22 +22,7 @@ public class FrogIA : EnemyController
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
             transform.GetComponent<Animator>().SetBool("IsWalk", false);
         }
-        if (life <= 0) {
-			transform.GetComponent<Animator>().SetBool("IsDead", true);
-			StartCoroutine(DestroyEnemy());
-		}
+  
         
     }
-
-    IEnumerator DestroyEnemy()
-	{
-		CapsuleCollider2D capsule = GetComponent<CapsuleCollider2D>();
-		capsule.size = new Vector2(1f, 0.25f);
-		capsule.offset = new Vector2(0f, -0.8f);
-		capsule.direction = CapsuleDirection2D.Horizontal;
-		yield return new WaitForSeconds(0.25f);
-		rb2d.velocity = new Vector2(0, rb2d.velocity.y);
-		yield return new WaitForSeconds(1f);
-		Destroy(gameObject);
-	}
 }

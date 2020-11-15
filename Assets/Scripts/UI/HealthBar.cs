87480de525776146d4eac219130df49f79339205
Allan Schramm <3208077+phodastick@users.noreply.github.com>
2020-11-15 17:56:04 -0300

    Healthbar UI fixed

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -22,17 +22,27 @@ public class HealthBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //Controla primeiro coração
         if(player.life >= 0 && player.life <= 4){
             healthBarUI1.sprite = bar[player.life];
         }
+        if(player.life >= 5){
+            healthBarUI1.sprite = bar[4];
+        }
 
+        // Controla segundo coração
         if(player.life >= 5 && player.life <= 8){
             healthBarUI2.sprite = bar[player.life-4];
         }
         else if(player.life <= 4){
             healthBarUI2.sprite = bar[0];
         }
+        if(player.life >= 9){
+            healthBarUI2.sprite = bar[4];
+        }
 
+        // Controla terceiro coração
         if(player.life >= 9 && player.life <= 12){
             healthBarUI3.sprite = bar[player.life-8];
         }
